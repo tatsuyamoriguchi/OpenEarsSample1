730473a75806bc0e855d3c76076241e37c113f9a
Brian Moriguchi <brian@beckos.com>
2020-01-31 00:37:18 -0800

    Initial Commit

---
+++ OpenEarsSample1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OpenEarsSample1
+//
+//  Created by Tatsuya Moriguchi on 1/31/20.
+//  Copyright © 2020 Tatsuya Moriguchi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
