d2ce44eb52b008631ca8613c6775716036d2263e
Brian Moriguchi <brian@beckos.com>
2020-02-04 00:50:39 -0800

    Initial commit

---
+++ OpenEarsHeader.h
@@ -0,0 +1,21 @@
+//
+//  OpenEarsHeader.h
+//  OpenEarsSample1
+//
+//  Created by Tatsuya Moriguchi on 1/31/20.
+//  Copyright © 2020 Tatsuya Moriguchi. All rights reserved.
+//
+
+
+#ifndef OpenEarsHeader_h
+#define OpenEarsHeader_h
+
+#import <OpenEars/OEPocketsphinxController.h>
+#import <OpenEars/OELanguageModelGenerator.h>
+#import <OpenEars/OEFliteController.h>
+#import <Slt/Slt.h> // Only needed if using Flite speech
+#import <OpenEars/OEEventsObserver.h>
+#import <OpenEars/OELogging.h>
+#import <OpenEars/OEAcousticModel.h>
+
+#endif /* OpenEarsHeader_h */   
